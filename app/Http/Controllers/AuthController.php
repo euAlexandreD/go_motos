affa2133c694b0e6400242ca121632e36beab462
Alexandre Portella <eualexandreeps@gmail.com>
2026-04-14 12:57:40 -0300

    removido a .env do gitignore

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,33 +15,35 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-           $request->validate([
-            'text_email' => 'required|email',
-            'text_password' => 'required|min:6|max:16',
-        ],
-        [
-            'text_email.required' => 'E-mail é obrigatório',
-            'text_email.email' => 'E-mail precisa ser um email válido',
-            'text_password.required' => 'Password é obrigatório',
-            'text_password.min' => 'Password deve ter pelo menos 6 caracteres',
-            'text_password.max' => 'Password não deve exceder 16 caracteres',
-        ]);
+        $request->validate(
+            [
+                'text_email' => 'required|email',
+                'text_password' => 'required|min:6|max:16',
+            ],
+            [
+                'text_email.required' => 'E-mail é obrigatório',
+                'text_email.email' => 'E-mail precisa ser um email válido',
+                'text_password.required' => 'Password é obrigatório',
+                'text_password.min' => 'Password deve ter pelo menos 6 caracteres',
+                'text_password.max' => 'Password não deve exceder 16 caracteres',
+            ]
+        );
 
         $useremail = $request->input('text_email');
         $password = $request->input('text_password');
 
         $user = User::where('email', $useremail)
-                    ->where('deleted_at', NULL)
-                    ->first();
+            ->where('deleted_at', NULL)
+            ->first();
 
-        if(!$user){
+        if (!$user) {
             return redirect()
                 ->back()
                 ->withInput()
                 ->with('loginError', 'Credenciais invalidas');
         }
 
-        if(!password_verify($password, $user->password)){
+        if (!password_verify($password, $user->password)) {
             return redirect()
                 ->back()
                 ->withInput()
@@ -60,7 +62,7 @@ class AuthController extends Controller
         return redirect()->to('/');
     }
 
-     public function logout()
+    public function logout()
     {
         session()->forget('user');
         return redirect()->to('/');
